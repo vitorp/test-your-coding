c5533ef6f52fbc84f787e2c321e3aaaef272f874
vitor pontes <vitormax2005@hotmail.com>
2016-08-22 21:53:53 -0300

    Ajuste de comentário

--- ruby/elem3.rb
+++ ruby/elem3.rb
@@ -1,6 +1,6 @@
 puts "Digite seu nome:"
 nome = gets.chomp
-nome.downcase! # Coloca o nome em lestras minúsculas para efeito de comparação
+nome.downcase! # Coloca o nome em letras minúsculas para efeito de comparação
 if nome == "alice" or nome == "bob" then
   puts "Olá " + nome +"!"
 end
