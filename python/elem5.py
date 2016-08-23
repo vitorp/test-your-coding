0f012da19e3db2549ee7e0c67d8de1ce40602801
vitor pontes <vitormax2005@hotmail.com>
2016-08-22 22:16:41 -0300

    Python elementar 3,4 e 5

---
+++ python/elem5.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+# int() realiza o typecasting de string para inteiro
+num = int(raw_input("Digite um número: "))
+soma = 0
+# "range(1,num)" é um range dos números de '1' a 'num-1'
+for iterador in range(1,num+1):
+  if iterador % 3 == 0 or iterador % 5 == 0:
+    soma += iterador
+
+print ("Soma de 1 a %d é %d " % (num,soma))
